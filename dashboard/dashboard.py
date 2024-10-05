6f38997c9ce29b97bb6954313f44f1e20c5078a7
ilmanvl <142105502+ilmanvl@users.noreply.github.com>
2024-10-05 17:07:03 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -31,7 +31,7 @@ def create_by_weekday_df(df):
     return by_weekday_df
 
 # Membaca data dari CSV
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.read_csv("dashboard/all_data.csv")
 
 # Mengonversi kolom tanggal menjadi datetime
 all_df['dteday'] = pd.to_datetime(all_df['dteday'])
